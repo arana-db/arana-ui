dea1bbf6c8873efbab56f595205f76795a953770
GavinLam164 <m13590491852@163.com>
2022-08-11 03:20:57 +0800

    chore: remove monaco-editor

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -1,30 +1,20 @@
-import { getOverview, getOverviewTree } from '@/services/ant-design-pro/api';
+import { getOverviewTree } from '@/services/ant-design-pro/api';
 import { PageContainer } from '@ant-design/pro-components';
 import { Card, message, Popover, Tabs, Tree } from 'antd';
 
 import React, { useEffect, useState } from 'react';
-import MonacoEditor from 'react-monaco-editor';
+// import MonacoEditor from 'react-monaco-editor';
 const { TabPane } = Tabs;
 
-const EDITOR_OPTIONS: any = {
-  lineNumbers: 'off',
-  minimap: {
-    enabled: false,
-  },
-  readonly: true,
-  quickSuggestions: true,
-  wordWrap: 'ff',
-};
-
 const Welcome: React.FC = () => {
-  const [overview, setOverview] = useState<Record<string, any> | null>(null);
+  // const [overview, setOverview] = useState<Record<string, any> | null>(null);
   const [overviewTree, setOverviewTree] = useState<any[] | undefined>(undefined);
   useEffect(() => {
     (async () => {
       const hide = message.loading('loading');
-      const res = await getOverview();
+      // const res = await getOverview();
       hide();
-      setOverview(res);
+      // setOverview(res);
     })();
     (async () => {
       const hide = message.loading('loading');
@@ -52,14 +42,14 @@ const Welcome: React.FC = () => {
             )}
           </TabPane>
           <TabPane tab="JSON" key="3" style={{ width: '100%' }}>
-            <MonacoEditor
+            {/* <MonacoEditor
               width={'100%'}
               height={400}
               language="json"
               theme="vs"
               value={JSON.stringify(overview, null, 4)}
               options={EDITOR_OPTIONS}
-            />
+            /> */}
           </TabPane>
         </Tabs>
       </Card>
